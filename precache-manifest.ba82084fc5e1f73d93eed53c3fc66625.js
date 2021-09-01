4c1f4ae4adcd25e3559215294461980b443d5cf7
Theng <Theng@users.noreply.github.com>
2021-09-01 06:42:29 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.ba82084fc5e1f73d93eed53c3fc66625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118fc3f6ea251925d77346be4e514f51",
+    "url": "https://theng.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "eca0c59da26095c3f184",
+    "url": "https://theng.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ae7a758177fc82eab6e4",
+    "url": "https://theng.github.io/task-management-frontend/static/js/2.0b32cf59.chunk.js"
+  },
+  {
+    "revision": "eca0c59da26095c3f184",
+    "url": "https://theng.github.io/task-management-frontend/static/js/main.b04ffe04.chunk.js"
+  },
+  {
+    "revision": "14c0b36a3ecb125615b6",
+    "url": "https://theng.github.io/task-management-frontend/static/js/runtime~main.d4e9f2ed.js"
+  }
+]);
